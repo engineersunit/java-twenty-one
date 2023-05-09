e6d85ef3caaa2b8ca78b4a55969cdb88833f0454
Sunit Ghosh <engineersunit@gmail.com>
2023-05-09 21:43:10 +0530

    Java 21 - JEP 431 Sequenced Collections Live Code

--- src/main/java/org/sun/ghosh/java21/SequencedCollections.java
+++ src/main/java/org/sun/ghosh/java21/SequencedCollections.java
@@ -3,8 +3,34 @@ package org.sun.ghosh.java21;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * JEP 431: Sequenced Collections: https://openjdk.org/jeps/431
+ * A sequenced collection is a Collection whose
+ * elements have a defined encounter order.
+ *
+ * JDK 21 Home Page: https://openjdk.org/projects/jdk/21/
+ * OpenJDK JDK 21 Early-Access Builds: https://jdk.java.net/21/
+ * OpenJDK SequencedCollections Code Changes: https://github.com/openjdk/jdk/commit/17ce0976e442d5fabb14daed40fa9a768989f02e
+ */
 public class SequencedCollections {
     public static void main(String[] args) {
+        /**
+         * interface SequencedCollection<E>
+         *     extends Collection<E> {
+         *
+         *     // new method
+         *     SequencedCollection<E> reversed();
+         *
+         *     // methods promoted from Deque
+         *     void addFirst(E);
+         *     void addLast(E);
+         *     E getFirst();
+         *     E getLast();
+         *     E removeFirst();
+         *     E removeLast();
+         * }
+         */
+
         List<String> listOfSkills = new ArrayList<>();
 
         listOfSkills.add("Oracle Database");
