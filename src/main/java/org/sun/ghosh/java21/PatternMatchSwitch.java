0be5c7ce7bb5748b2f8d4aa65195bd19aab27004
Sunit Ghosh <engineersunit@gmail.com>
2023-07-02 10:53:51 +0530

    JEP 441: Pattern Matching for switch

--- src/main/java/org/sun/ghosh/java21/PatternMatchSwitch.java
+++ src/main/java/org/sun/ghosh/java21/PatternMatchSwitch.java
@@ -9,15 +9,19 @@ import static java.lang.System.out;
 
 // JEP 445: Unnamed Classes and Instance Main Methods (Preview)
 void main(){
-        out.println(formatValue("Java 21"));
-        out.println(format21Value(Integer.MAX_VALUE));
+        print(formatValue("Java 21"));
+        print(format21Value(Integer.MAX_VALUE));
         generateResponse(null);
         generate21Response("Hello");
         testInput("Yes");
         test21Input("No");
-        test21InputEnhanced(null);
+        print(validateInputAndRespond(null));
         exhaustiveSwitchWithoutEnumSupport(IndoorGame.BALL);
         exhaustiveSwitchWithBetterEnumSupport(IndoorGame.PUZZLE);
+}
+
+static void print(Object object){
+        out.println(object);
 }
 
 /**
@@ -60,12 +64,12 @@ static String format21Value(Object obj) {
  */
 static void generateResponse(String s) {
         if (s == null) {
-            out.println("Nothing to say!");
+            print("Nothing to say!");
             return;
         }
         switch (s) {
-        case "Hello", "Bye" -> out.println("See you!");
-        default             -> out.println("Same to you!");
+        case "Hello", "Bye" -> print("See you!");
+        default             -> print("Same to you!");
         }
 }
 
@@ -74,9 +78,9 @@ static void generateResponse(String s) {
  */
 static void generate21Response(String s) {
         switch (s) {
-        case null           -> out.println("Nothing to say!");
-        case "Hello", "Bye" -> out.println("See you!");
-        default             -> out.println("Same to you!");
+        case null           -> print("Nothing to say!");
+        case "Hello", "Bye" -> print("See you!");
+        default             -> print("Same to you!");
         }
 }
 
@@ -88,14 +92,14 @@ static void generate21Response(String s) {
  */
 static void testInput(String response) {
         switch (response) {
-        case null -> { out.println("Nothing to say!"); }
+        case null -> print("Nothing to say!");
         case String s -> {
             if (s.equalsIgnoreCase("YES"))
-                out.println("Welcome!");
+                print("Welcome!");
             else if (s.equalsIgnoreCase("NO"))
-                out.println("Good Bye!");
+                print("Good Bye!");
             else
-                out.println("Retry!");
+                print("Retry!");
         }
     }
 }
@@ -107,17 +111,17 @@ static void testInput(String response) {
  */
 static void test21Input(String response) {
         switch (response) {
-        case null -> { out.println("Nothing to say!"); }
+        case null -> print("Nothing to say!");
         case String s
         when s.equalsIgnoreCase("YES") -> {
-                out.println("Welcome!");
+                print("Welcome!");
         }
         case String s
         when s.equalsIgnoreCase("NO") -> {
-                out.println("Good Bye!");
+                print("Good Bye!");
         }
         case String s -> {
-                out.println("Retry!");
+                print("Retry!");
         }
 }
 }
@@ -130,27 +134,17 @@ static void test21Input(String response) {
  * the logic that applies if that test is satisfied is
  * on the right of a switch rule
  */
-static void test21InputEnhanced(String response) {
-        switch (response) {
-        case null -> { out.println("Nothing to say!"); }
-        case "y", "Y" -> {
-                out.println("Welcome!");
-        }
-        case "n", "N" -> {
-                out.println("Good Bye!");
-        }
-        case String s
-        when s.equalsIgnoreCase("YES") -> {
-                out.println("Welcome!");
-        }
-        case String s
-        when s.equalsIgnoreCase("NO") -> {
-                out.println("Good Bye!");
-        }
-        case String s -> {
-                out.println("Retry!");
-        }
-        }
+static String validateInputAndRespond(String response) {
+        return switch (response) {
+        case null     -> "Nothing to say!";
+        case "y", "Y" -> "Welcome!";
+        case "n", "N" -> "Good Bye!";
+        case String s when
+         s.equalsIgnoreCase("YES") -> "Welcome!";
+        case String s when
+         s.equalsIgnoreCase("NO")  -> "Good Bye!";
+        case String s -> "Retry!";
+        };
 }
 
 /**
@@ -159,7 +153,7 @@ static void test21InputEnhanced(String response) {
 sealed interface GameClassification
         permits IndoorGame, OutdoorGame {}
 public enum IndoorGame implements GameClassification
-{ BOARD, BALL, VIDEO, PUZZLE }
+        { BOARD, BALL, VIDEO, PUZZLE }
 final class OutdoorGame implements GameClassification {}
 
 /** Java 21
@@ -174,21 +168,17 @@ final class OutdoorGame implements GameClassification {}
 
 static void exhaustiveSwitchWithoutEnumSupport(GameClassification c) {
         switch (c) {
-                case IndoorGame s when s == IndoorGame.BOARD -> {
-                        out.println("It's BOARD");
-                }
-                case IndoorGame s when s == IndoorGame.BALL -> {
-                        out.println("It's BALL");
-                }
-                case IndoorGame s when s == IndoorGame.VIDEO -> {
-                        out.println("It's VIDEO");
-                }
-                case IndoorGame s -> {
-                        out.println("It's PUZZLE");
-                }
-                case OutdoorGame t -> {
-                        out.println("It's a OutdoorGame");
-                }
+                case IndoorGame s when
+                        s == IndoorGame.BOARD ->
+                         print("It's BOARD");
+                case IndoorGame s when
+                        s == IndoorGame.BALL  ->
+                         print("It's BALL");
+                case IndoorGame s
+                        when s == IndoorGame.VIDEO ->
+                         print("It's VIDEO");
+                case IndoorGame s  -> print("It's PUZZLE");
+                case OutdoorGame t -> print("It's a OutdoorGame");
         }
 }
 
@@ -204,21 +194,15 @@ static void exhaustiveSwitchWithoutEnumSupport(GameClassification c) {
  */
 static void exhaustiveSwitchWithBetterEnumSupport(GameClassification c) {
         switch (c) {
-                case IndoorGame.BOARD -> {
-                        out.println("It's BOARD");
-                }
-                case IndoorGame.BALL -> {
-                        out.println("It's BALL");
-                }
-                case IndoorGame.VIDEO -> {
-                        out.println("It's VIDEO");
-                }
-                case IndoorGame.PUZZLE -> {
-                        out.println("It's PUZZLE");
-                }
-                case OutdoorGame t -> {
-                        out.println("It's a OutdoorGame");
-                }
+                case IndoorGame.BOARD ->
+                        print("It's BOARD");
+                case IndoorGame.BALL  ->
+                        print("It's BALL");
+                case IndoorGame.VIDEO ->
+                        print("It's VIDEO");
+                case IndoorGame.PUZZLE->
+                        print("It's PUZZLE");
+                case OutdoorGame t    ->
+                        print("It's a OutdoorGame");
         }
 }
-
